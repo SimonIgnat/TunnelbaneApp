4b05830df043c84e87d0edb4eef9e94c950dfe8e
simonignat <signat@kth.se>
2015-04-18 20:54:06 +0200

    Init
    Created this class
    
    Created URL makers and URL->XML stream
    
    Added some shit

--- app/src/main/java/com/example/simonignat/tunnelbaneapp/XMLParser.java
+++ app/src/main/java/com/example/simonignat/tunnelbaneapp/XMLParser.java
@@ -1,9 +1,11 @@
 package com.example.simonignat.tunnelbaneapp;
-import android.util.Log;
-import android.widget.TextView;
+
+import android.location.Location;
+
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -11,15 +13,83 @@ import java.util.ArrayList;
 
 public class XMLParser {
 
-    public TextView tv;
-    String destination = "";
-    String lineNumber = "";
-    String time = "";
-    String tag = "";
-    ArrayList<Transport> transport =new ArrayList<Transport>();
+    private String destination = "";
+    private String lineNumber = "";
+    private String time = "";
+    private String tag = "";
+    private ArrayList<Transport> transport =new ArrayList<Transport>();
 
     public XMLParser(){
     }
+    public Site getSiteInfo(InputStream stream) throws XmlPullParserException, IOException {
+        XmlPullParserFactory xmlFactoryObject = XmlPullParserFactory.newInstance();
+        XmlPullParser parser = xmlFactoryObject.newPullParser();
+
+        parser.setInput(stream, null);
+        int event = parser.getEventType();
+        String tag = "";
+        while (event != XmlPullParser.END_DOCUMENT) {
+
+            switch (event) {
+                case XmlPullParser.START_TAG:
+                    tag = parser.getName();
+                    if(tag.equals("Site")) {
+                        parseSiteData(parser);
+                        //Log.w("parser")
+                        Location loc =new Location(locationName);
+                        loc.setLatitude(locationY);
+                        loc.setLongitude(locationX);
+                        return new Site(siteId, locationName,loc);
+                    } break;
+            }
+            event = parser.next();
+        }
+        return null;
+    }
+
+    private int siteId;
+    private String locationName;
+    private int locationX;
+    private int locationY;
+    private void parseSiteData(XmlPullParser parser) throws XmlPullParserException,IOException{
+
+
+        int event = parser.getEventType();
+        boolean go=true;
+        while (go) {
+            String name = parser.getName();
+            switch (event) {
+                case XmlPullParser.START_TAG:
+                    tag = parser.getName();
+                    break;
+                case XmlPullParser.TEXT:
+                    if (tag.equals("SiteId"))
+                        siteId = Integer.valueOf(parser.getText());
+
+                    if (tag.equals("Name"))
+                        locationName = parser.getText();
+
+                    if (tag.equals("X")) {
+                        locationX = Integer.valueOf(parser.getText());
+                    }
+                    if (tag.equals("Y")) {
+                        locationY = Integer.valueOf(parser.getText());
+                    }
+                    break;
+
+                case XmlPullParser.END_TAG:
+                    tag="--";
+                    if(parser.getName().equals("Site")){
+                        go=false;
+                    }
+                    break;
+            }
+            if(go)
+                event = parser.next();
+        }
+    }
+
+
 
     public ArrayList<Transport> parse(InputStream stream) throws XmlPullParserException, IOException {
 
