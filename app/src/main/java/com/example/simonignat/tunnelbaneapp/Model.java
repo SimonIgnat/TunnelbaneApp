35419bc5d8f739eb0917bb5dccd1aacb0336432f
simonignat <signat@kth.se>
2015-04-21 10:55:54 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	app/src/main/java/com/example/simonignat/tunnelbaneapp/XMLParser.java
    
    Changed GUI and sent ContUpdate instead of model (ContUpdate includes a static reference to model)
    
    Changed minimum API to 19 to avoid errors when creating XML-stream.
    
    Corrected nextTrainToString()
    
    Changed quite a bit. But now we're able to show the information on mainActivity. Current bugs:
    - User has to press the button two times for the information to show the first time
    
    Removed all the unnecessary static's
    
    Added image and made the keyboard disappear when button pressed
    
    Made updatedeparture run from the same thread as init-site.

--- app/src/main/java/com/example/simonignat/tunnelbaneapp/Model.java
+++ app/src/main/java/com/example/simonignat/tunnelbaneapp/Model.java
@@ -2,6 +2,7 @@ package com.example.simonignat.tunnelbaneapp;
 
 import android.os.Parcelable;
 import android.util.Log;
+import android.widget.TextView;
 
 import java.io.ByteArrayInputStream;
 import java.nio.charset.StandardCharsets;
@@ -12,10 +13,15 @@ import static java.nio.charset.StandardCharsets.*;
 /**
  * Created by simonignat on 2015-04-18.
  */
-public class Model{
+
+
+public class Model {
+
+    //Push Test malte!
     ArrayList<Transport> transports = new ArrayList<Transport>();
 
-    Site currentSite = null;
+
+    Site currentSite;
 
     URLRequest siteAPIHandle = null;
     URLRequest departuresAPIHandle = null;
@@ -29,21 +35,25 @@ public class Model{
 
     XMLParser xmlParser = new XMLParser();
 
-   // final String UI = null;
-    private int timeStep;
+    // final String UI = null;
+    private int timeStep = 50;
 
 
-    Model(String uI){
+    TextView busText;
+    TextView metroText;
+    TextView siteTitle;
 
+    Model(String uI, TextView busText,TextView metroText) {
         //final String userInput = uI;
         //this.currentSite = new Site(9000, "Central Stationen", null);
-
+        this.busText=busText;
+        this.metroText=metroText;
         setUserInput(uI);
         initSite();
 
     }
 
-    void initSite(){
+    void initSite() {
 
         /**
          * BEHÖVS TRÅDAR????
@@ -53,42 +63,78 @@ public class Model{
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-                String siteURL = SiteURLCreator.createURL(getUserInput());
 
-
+                //String siteURL = SiteURLCreator.createURL(getUserInput());
                 try {
+
+                    //HARDCODED FOR NOW
+                    String siteURL = "http://api.sl.se/api2/typeahead.xml?key=f7a9ba6bee8a46f9a8d07629719d3935&searchstring=Tekniskahogskolan&stationsonly=true&maxresults=10";
 
                     siteAPIHandle = new URLRequest(siteURL);
 
                     siteXMLStream = siteAPIHandle.getResponseFromUrl();
 
+                    Site test = xmlParser.getSiteInfo(new ByteArrayInputStream(siteXMLStream.getBytes(StandardCharsets.UTF_8)));
+
+                    Log.w("test", test.toString());
+
                     setCurrentSite(xmlParser.getSiteInfo(new ByteArrayInputStream(siteXMLStream.getBytes(StandardCharsets.UTF_8))));
 
-                }catch(Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+                    //HARDCODED FOR NOW
+                    // String departureURL = "http://api.sl.se/api2/realtimedepartures.xml?key=985c280f5aab414d9584b8a58230a386&siteid=9192&timewindow=15"
 
-        thread.start();
-    }
+                    Thread departureFetcher = new Thread(new Runnable() {
 
-    void updateDepartures(){
+                        @Override
+                        public void run(){
+                            try{
+                                //String departureURL = DepartureURLCreator.createURL(getCurrentSite());
 
 
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
+                                String departureURL = "http://api.sl.se/api2/realtimedepartures.xml?key=985c280f5aab414d9584b8a58230a386&siteid=9204&timewindow=15";
 
-                String departureURL = DepartureURLCreator.createURL(getCurrentSite());
+                                departuresAPIHandle = new URLRequest(departureURL);
 
-                try {
+                                departuresXMLStream = departuresAPIHandle.getResponseFromUrl();
 
-                    departuresAPIHandle = new URLRequest(departureURL);
 
-                    departuresXMLStream = departuresAPIHandle.getResponseFromUrl();
-                    setTransports(xmlParser.getDepartures(new ByteArrayInputStream(departuresXMLStream.getBytes(StandardCharsets.UTF_8))));
+                                setTransports(xmlParser.getDepartures(new ByteArrayInputStream(departuresXMLStream.getBytes(StandardCharsets.UTF_8))));
+
+
+                            }catch(Exception e){
+                                Log.w("e2","E2");
+                                e.printStackTrace();
+                            }
+
+
+                            Runnable runOnMainThrad = new Runnable(){
+                                public void run(){
+                                    busText.setText(nextBusToString());
+                                }
+                            };
+
+                            Runnable runOnMainThrad2 = new Runnable(){
+
+                                public void run(){
+                                    metroText.setText(nextMetroToString());
+                                }
+                            };
+
+
+                            //Updating view.
+
+                            busText.post(runOnMainThrad);
+                            metroText.post((runOnMainThrad2));
+                        }});
+
+
+                    departureFetcher.start();
+
+
                 } catch (Exception e) {
+
+                    Log.w("e1","E1");
+
                     e.printStackTrace();
                 }
 
@@ -98,68 +144,53 @@ public class Model{
         thread.start();
     }
 
-    public long getTimeStep() {
+
+
+    long getTimeStep() {
         return timeStep;
     }
 
-    public void setTimeStep(int s){
+    void setTimeStep(int s) {
         timeStep = s;
     }
 
-    public Site getSite(){
-        return currentSite;
 
-    }
 
-    public String nextBusToString(){
-        Bus b = null;
+    String nextMetroToString() {
 
-        for(Transport t: transports){
-            try {
-                b = (Bus) t;
-            }catch(ClassCastException e){
-                e.printStackTrace();
+        for (Transport t : transports) {
+            if (t.getClass().equals(Metro.class)) {
+                String string = "Tunnelbana: " + t.getName() + "  -  "  + t.getTime();
+                return string;
             }
         }
-        String string = b.getName() + b.getTime();
-        return string;
+        return " ";
     }
 
-    public String nextMetroToString(){
-        Metro m = null;
 
-        for(Transport t: transports){
-            try {
-                m = (Metro) t;
-            }catch(ClassCastException e){
-                e.printStackTrace();
+    String nextBusToString() {
+
+        for (Transport t : transports) {
+            if (t.getClass().equals(Bus.class)) {
+                return "Buss: " + t.getName() + "  -  "  + t.getTime();
             }
         }
-        String string = m.getName() + m.getTime();
-        return string;
+        return "";
     }
 
-    public String nextTrainToString(){
-        Metro tr = null;
 
-        for(Transport t: transports) {
-            try {
-                tr = (Metro) t;
-            } catch (ClassCastException e) {
-                e.printStackTrace();
+    String nextTrainToString() {
+
+        for (Transport t : transports) {
+            if (t.getClass().equals(Train.class)) {
+                return "Pendeltåg: " + t.getName() + "  -  "  + t.getTime();
             }
         }
-
-        String string = tr.getName() + tr.getTime();
-
-
-
-        return string;
+        return " ";
     }
 
 
     void setCurrentSite(Site site){
-
         currentSite = site;
     }
 
@@ -172,11 +203,10 @@ public class Model{
         transports = transport;
     }
 
-    public void setUserInput(String userinput) {
-        this.userInput = userinput;
+    void setUserInput(String userinput) {
+        userInput = userinput;
     }
 
-    public String getUserInput(){
+    String getUserInput(){
         return this.userInput;
-    }
-}
+    }}
